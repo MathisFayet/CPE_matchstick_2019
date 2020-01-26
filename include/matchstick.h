d851502cffe5caabee0dc9cc1497e8828fff02da
Fayet Mathis <fmathis@desktop-bi466o6.home>
2020-01-26 16:18:46 +0100

    Add include for compile all the project with .h

---
+++ include/matchstick.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_solostumper_3_2019
+** File description:
+** lol.h
+*/
+
+#ifndef MY_FUNCTION_STR_H
+#define MY_FUNCTION_STR_H
+
+#include <stdlib.h>
+
+#include "my.h"
+
+int main(int argc, char **argv);
+
+#endif /* !MY_FUNCTION_STR_H */
