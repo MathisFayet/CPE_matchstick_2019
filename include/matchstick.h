b77b14ab326c8fb306c21ca6bd31b93fb7b39ec2
Fayet Mathis <fmathis@localhost.localdomain>
2020-02-11 11:13:20 +0100

    Add include for compile better

--- include/matchstick.h
+++ include/matchstick.h
@@ -9,9 +9,15 @@
 #define MY_FUNCTION_STR_H
 
 #include <stdlib.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "my.h"
 
-int main(int argc, char **argv);
+int function_matchstick(char *first, char *second);
+void my_print_star(char **dest);
+char **create_stick(int first, char **dest);
+char *get_next_line(int fd);
+void print_game(int second, int first, char **dest);
 
 #endif /* !MY_FUNCTION_STR_H */
